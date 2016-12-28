c5b19c9e6ad82fd2dec02737c8eb31d26408f0eb
eikur <jorge.soriano.pinedo@gmail.com>
2016-12-28 18:42:51 +0100

    Creation of Entity and EntityManager classes

---
+++ Entity.h
@@ -0,0 +1,12 @@
+#ifndef __ENTITY_H__
+#define __ENTITY_H__
+
+class Entity {
+
+public: 
+	Entity(){}
+	~Entity(){}
+
+};
+
+#endif __ENTITY_H__
---
+++ EntityManager.h
@@ -0,0 +1,11 @@
+#ifndef __ENTITYMANAGER_H__
+#define __ENTITYMANAGER_H__
+
+class EntityManager {
+
+public:
+	EntityManager();
+	~EntityManager();
+};
+
+#endif // __ENTITYMANAGER_H__
